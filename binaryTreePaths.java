65b8dcc0f141b1f2d487b4d7d7474b06cbfb085e
kyleyiyang <kyleyiyang@gmail.com>
2020-11-23 22:45:23 -0500

    Update binaryTreePaths.java

--- binaryTreePaths.java
+++ binaryTreePaths.java
@@ -19,94 +19,43 @@ class Solution {
         if (root==null) return lst;
         int level = -1;
         //lst.add(String.valueOf(root.val));
-        traversal(root,lst, level,"",0);
+        traversal(root,lst, level,"");
         //System.out.println(lst);
         return lst;
     }
-    public static void traversal(TreeNode node, List<String> lst, int level, String s,int count) {
+    public static void traversal(TreeNode node, List<String> lst, int level, String s) {
         if (node!=null) {
             level++;
-            //String s="";
-            if (level==0) {
-                lst.add(String.valueOf(node.val));
-            } else {
-                if (level>=(lst.get(count).length()-1)/3) {
-                    s=lst.get(count);
-                } else if (level<(lst.get(count).length()-1)/3) {
-                    s=lst.get(count).substring(0,level);
-                    count+=lst.size();
+            int cont = 0;
+            if (level>0) {
+                String temp=lst.get(lst.size()-1);
+                s=temp;    
+                
+                for(int i=0; i<temp.length(); i++) {
+                   if(temp.charAt(i) == '>') {
+                      cont++;
+                       if (cont==level) {
+                         s=temp.substring(0,i-1);
+                         break;
+                        }
+                    } 
                 }
             }
-            //System.out.println("1st s="+s);
-            if (s=="") s=String.valueOf(node.val);
-            else s=s+"->"+String.valueOf(node.val);
-            System.out.println(s);
-            //if (level<s.length()-1) count++; 
-            System.out.println("count="+count);
-            if (count==lst.size()-1) {
-                lst.set(count,s);
+            if (level==0) s=String.valueOf(node.val);
+            else s+=("->"+String.valueOf(node.val));
+            //System.out.println("level="+level+"; s="+s);
+            //if (lst.size()>=1) System.out.println("s.length="+s.length()+"; pre-len="+lst.get(lst.size()-1).length());
+            //if (lst.size()>=1 && s.length()>lst.get(lst.size()-1).length()) {
+            if (lst.size()>=1 && cont<level) {
+                lst.set(lst.size()-1,s);
             } else {
-                lst.add(lst.size(),s);
+                lst.add(s);
             }
-            System.out.println("lst="+lst);
-            traversal(node.left,lst,level,s,count);
-            traversal(node.right,lst,level,s,count);
+            
+            //System.out.println(lst);
+            traversal(node.left,lst,level,s);
+            traversal(node.right,lst,level,s);
         }
     }
-}
-*******************************************************************
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> lst=new ArrayList<String>();
-        if (root==null) return lst;
-        int level = -1;
-        //lst.add(String.valueOf(root.val));
-        traversal(root,lst, level,"",0);
-        //System.out.println(lst);
-        return lst;
-    }
-    public static void traversal(TreeNode node, List<String> lst, int level, String s,int count) {
-        if (node!=null) {
-            level++;
-            //String s="";
-            if (level==0) {
-                lst.add(String.valueOf(node.val));
-            } else {
-                if (level>=lst.get(count).length()) {
-                    s=lst.get(count);
-                } else if (level<lst.get(count).length()) {
-                    s=lst.get(count).substring(0,level);
-                    count+=lst.size();
-                }
-            }
-            //System.out.println("1st s="+s);
-            s=s+String.valueOf(node.val);
-            System.out.println(s);
-            //if (level<s.length()-1) count++; 
-            System.out.println("count="+count);
-            if (count==lst.size()-1) {
-                lst.set(count,s);
-            } else {
-                lst.add(lst.size(),s);
-            }
-            System.out.println("lst="+lst);
-            traversal(node.left,lst,level,s,count);
-            traversal(node.right,lst,level,s,count);
-        }
-    }
+    
 }
